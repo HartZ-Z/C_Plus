71986b111ac94be85a2c849f222eef2641f8f7fa
HartZ-Z <ntchaotic@gmail.com>
2025-02-07 17:34:50 +0530

    armstrong

---
+++ Arm.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class arm
+{
+  private:
+    int n;
+  public:
+   void Arm (int x )
+    {
+      n=x;
+    }
+    void check () {
+      int t, c=0 , s=0;
+      t=n;
+      while (t!=0) {
+        t=t/10;
+        c++;
+      }
+      t=n;
+
+      while (t!=0){
+        int d;
+        d=t%10;
+        t=t/10;
+        s=s+ pow(d,c);
+
+      }
+    if (n == s){
+    cout<<"\n Given number is armstrong: "<<n ;
+
+    }
+
+    else {
+      cout<<"\n Given number is not armstrong: "<<n ;
+}
+
+}
+
+};
+
+int main (){
+  arm a;
+  int n;
+  cout<<"\n Enter a number you want to check: ";
+  cin>>n;
+  a.Arm(n);
+  a.check();
+
+}
